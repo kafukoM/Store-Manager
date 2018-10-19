b15e88cf4e66b1f59309b48744cea0c3e76b6cc5
kafukoM <kafukom2@gmail.com>
2018-10-19 22:43:45 +0200

    Add files via upload

---
+++ password.js
@@ -0,0 +1,18 @@
+<form name="sendin" action="$(link-login-only)" method="post">
+		<input type="hidden" name="username" />
+		<input type="hidden" name="password" />
+		<input type="hidden" name="dst" value="$(link-orig)" />
+		<input type="hidden" name="popup" value="true" />
+	</form>
+	<script type="text/javascript" src="/md5.js"></script>
+	<script type="text/javascript">
+	<!--
+	    function doLogin() {
+		document.sendin.username.value = document.login.username.value;
+		document.sendin.password.value = hexMD5('$(chap-id)' + document.login.password.value + '$(chap-challenge)');
+		document.sendin.submit();
+		return false;
+	    }
+    //-->
+    
+	</script>
